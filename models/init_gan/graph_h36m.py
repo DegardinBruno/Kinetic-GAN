442a5341f05ab86c33cbc4a8ddc4cfcbee3c236f
DegardinBruno <brunomdegardin@gmail.com>
2021-10-24 16:44:52 +0100

    Update comments

--- models/init_gan/graph_h36m.py
+++ models/init_gan/graph_h36m.py
@@ -3,33 +3,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class Graph_h36m():
-    """ The Graph to model the skeletons extracted by the openpose
-
-    Args:
-        strategy (string): must be one of the follow candidates
-        - uniform: Uniform Labeling
-        - distance: Distance Partitioning
-        - spatial: Spatial Configuration
-        For more information, please refer to the section 'Partition Strategies'
-            in our paper (https://arxiv.org/abs/1801.07455).
-
-        layout (string): must be one of the follow candidates
-        - openpose: Is consists of 18 joints. For more information, please
-            refer to https://github.com/CMU-Perceptual-Computing-Lab/openpose#output
-        - ntu-rgb+d: Is consists of 25 joints. For more information, please
-            refer to https://github.com/shahroudy/NTURGB-D
-
-        max_hop (int): the maximal distance between two connected nodes
-        dilation (int): controls the spacing between the kernel points
-
-    """
 
     def __init__(self,
                  max_hop=1,
                  dilation=1):
         self.max_hop  = max_hop
         self.dilation = dilation
-        self.lvls     = 4  # 25 -> 11 -> 5 -> 1
+        self.lvls     = 4  # 16 -> 7 -> 2 -> 1
         self.As       = []
         self.hop_dis  = []
 
--- models/init_gan/graph_ntu.py
+++ models/init_gan/graph_ntu.py
@@ -3,26 +3,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class graph_ntu():
-    """ The Graph to model the skeletons extracted by the openpose
-
-    Args:
-        strategy (string): must be one of the follow candidates
-        - uniform: Uniform Labeling
-        - distance: Distance Partitioning
-        - spatial: Spatial Configuration
-        For more information, please refer to the section 'Partition Strategies'
-            in our paper (https://arxiv.org/abs/1801.07455).
-
-        layout (string): must be one of the follow candidates
-        - openpose: Is consists of 18 joints. For more information, please
-            refer to https://github.com/CMU-Perceptual-Computing-Lab/openpose#output
-        - ntu-rgb+d: Is consists of 25 joints. For more information, please
-            refer to https://github.com/shahroudy/NTURGB-D
-
-        max_hop (int): the maximal distance between two connected nodes
-        dilation (int): controls the spacing between the kernel points
-
-    """
 
     def __init__(self,
                  max_hop=1,
